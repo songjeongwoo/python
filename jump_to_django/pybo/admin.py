952fe59e1635bbfc9a902398d42555d48faa5ead
songjeongwoo <nice920525@naver.com>
2022-02-06 16:07:06 +0900

    장고_관리자(검색기능추가)

--- jump_to_django/pybo/admin.py
+++ jump_to_django/pybo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Question
 
-# Register your models here.
+# 관리자 화면에 검색기능 추가
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+# admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
+
+
+# cf) 장고 관리자 기능: https://docs.djangoproject.com/en/3.0/ref/contrib/admin/
