c21271f6587c981cf256845db7a5694910f840be
songjeongwoo <nice920525@naver.com>
2022-02-06 16:52:07 +0900

    URL네임스페이스와_별칭사용

--- jump_to_django/pybo/urls.py
+++ jump_to_django/pybo/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+# URL 별칭 사용 시 중복을 피하기 위해 URL namespace를 설정
+app_name = 'pybo'
+
 urlpatterns = [
-    path('', views.index),
-    path('<int:question_id>/', views.detail),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
