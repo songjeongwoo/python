bb8f704bf71daf54abf0d695b60b6bf09a9552bd
songjeongwoo <nice920525@naver.com>
2022-02-27 16:28:21 +0900

    str.count()

---
+++ basic/str.count.py
@@ -0,0 +1,7 @@
+# 정의된 문자열(String)에서 해당문자가 몇번 존재하는지 카운트하는 방법
+
+w = 'You can count me how many times, more and more'
+w.count('o')  # 5
+w.count('more')  # 2
+
+# 출처: https://dongyeopblog.wordpress.com/2016/06/24/python-%EB%AC%B8%EC%9E%90%EC%97%B4%EC%97%90%EC%84%9C-%ED%8A%B9%EC%A0%95%EB%AC%B8%EC%9E%90-%EC%B9%B4%EC%9A%B4%ED%8A%B8%ED%95%98%EB%8A%94%EB%B2%95/
